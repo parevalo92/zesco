611327449f6ba86f29f46173e0bfed90d6538f5e
parevalo92 <85068855+parevalo92@users.noreply.github.com>
2025-08-30 20:11:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+//Toggle mobile menu
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//Close menu whe the close button is clicked
+    menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//Responsives Breakpoints
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
